1b2f0bbb8d666b0bf1471519bf04f419065f3a72
Bubsi <95187459+AnimeBubsi@users.noreply.github.com>
2024-02-07 17:30:29 +0200

    Add files via upload

---
+++ items/items.lua
@@ -0,0 +1,24 @@
+    ['oxygenmask'] =  {
+        ['name'] =  'oxygenmask',
+        ['label'] =  'Happinaamari',
+        ['weight'] =  300,
+        ['type'] =  'item',
+        ['image'] =  'oxygenmask.png',
+        ['unique'] =  false,
+        ['useable'] =  true,
+        ['shouldClose'] =  true,
+        ['combinable'] =  nil,
+        ['description'] =  '"Sukellus hommia ;)"'
+    },
+    ['emptyoxygenmask'] =  {
+        ['name'] =  'emptyoxygenmask',
+        ['label'] =  'Tyhjä Sukellustankki',
+        ['weight'] =  300,
+        ['type'] =  'item',
+        ['image'] =  'emptyoxygenmask.png',
+        ['unique'] =  false,
+        ['useable'] =  false,
+        ['shouldClose'] =  true,
+        ['combinable'] =  nil,
+        ['description'] =  'Voit täyttää tämän putelin'
+    },
---
+++ items/sql.sql
@@ -0,0 +1,3 @@
+INSERT INTO `ìtems`(name, label, weight, rare, can_remove) VALUES
+('oxygemask', 'Oxygenmask', 1,1,1),
+('emptyoxygenmask', 'Empty Oxygenmask', 1,1,1);
