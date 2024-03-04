387a781c19c485cd749c099f2533a2604f8d9ffb
Bubsi <95187459+AnimeBubsi@users.noreply.github.com>
2024-03-04 13:44:26 +0200

    Update Clientti.lua
    update

--- Clientti.lua
+++ Clientti.lua
@@ -9,7 +9,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 callback = QBCore.Functions.TriggerCallback
 end
 
-local ootaha, refill = false, false
+local ootaha, refill, auki = false, false, false
 
 function loadAnimDict(dict) 
     while (not HasAnimDictLoaded(dict)) do
@@ -18,39 +18,33 @@ function loadAnimDict(dict)
     end
 end
 
-
 Citizen.CreateThread(function()
     while true do
         local Bubsi = 500
         local maa = GetEntityHeightAboveGround(PlayerPedId())
         if IsPedSwimmingUnderWater(PlayerPedId()) and maa < 2.0 and ootaha == false then
 	Bubsi = 5
-            lib.showTextUI(Config.locales.tutki, {
-                position = "left-center",
-                icon = 'hand',
-                style = {
-                    borderRadius = 0,
-                    backgroundColor = '#48BB78',
-                    color = 'white'
-                }
-            })
-		if IsControlJustPressed(0, 38) and ootaha == false then
+	textui('show', Config.locales.tutki)
+	auki = true
+ 		if IsControlJustPressed(0, 38) and ootaha == false then
 		   ootaha = true
 		   loadAnimDict( "random@domestic" )
 	TaskPlayAnim( PlayerPedId(), "random@domestic", "pickup_low", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
 	Citizen.Wait(2000)
 	ClearPedTasksImmediately(PlayerPedId())
 		   palkkio('Bubsipalkkio')
-
-        elseif maa > 1.0 then
-            lib.hideTextUI()
+            end
+         end
+	  if maa > 2.0 and auki or ootaha and auki then
+	  textui('hide')
+          auki = false
 	   end
-        end
 	Citizen.Wait(Bubsi)
     end
 end)
 
-function palkkio(event)
+
+palkkio = function(event)
 local Bubsikey = 'Bubsinkeyjuttusysteemi' -- somekinda protect system?
 TriggerServerEvent(event, Bubsikey)
 end
@@ -71,7 +65,7 @@ AddEventHandler('Bubsimaski', function()
 	loadAnimDict( "clothingshirt" )
 	TaskPlayAnim( PlayerPedId(), "clothingshirt", "try_shirt_positive_d", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
 	if Config.useprogress then
-	Config.Progressbars(4000, Config.locales.progressbars)
+	Progressbars(4000, Config.locales.progressbars)
 	end
 	Citizen.Wait(4000)
 	ClearPedTasksImmediately(PlayerPedId())
@@ -82,15 +76,15 @@ AddEventHandler('Bubsimaski', function()
             			local bone1 = GetPedBoneIndex(PlayerPedId(), 24818)
             			AttachEntityToEntity(Scubatank, PlayerPedId(), bone1, -0.29, -0.23, 0.0, 180.0, 90.0, 0.0, 1, 1, 0, 0, 2, 1)
 				SetPedDiesInWater(PlayerPedId(), false)
-				Config.notification(5000, Config.locales.happea100, 'Clientti', Config.types.info)
+				TriggerEvent('Bubsinotify', 5000, Config.locales.happea100, Config.types.info)
 				Wait (100000)
-				Config.notification(5000, Config.locales.happea50, 'Clientti', Config.types.info)
+				TriggerEvent('Bubsinotify', 5000, Config.locales.happea50, Config.types.info)
 				Wait (85000)
-				Config.notification(5000, Config.locales.happea25, 'Clientti', Config.types.info)
+				TriggerEvent('Bubsinotify', 5000, Config.locales.happea25, Config.types.info)
 				Wait (15000)
-				Config.notification(5000, Config.locales.happea5, 'Clientti', Config.types.info)
+				TriggerEvent('Bubsinotify', 5000, Config.locales.happea5, Config.types.info)
 				Wait (5000)
-				Config.notification(5000, Config.locales.happea0, 'Clientti', Config.types.info)
+				TriggerEvent('Bubsinotify', 5000, Config.locales.happea0, Config.types.info)
 				ClearPedScubaGearVariation(PlayerPedId())
 				DeleteObject(Scubatank)	
 				SetPedDiesInWater(PlayerPedId(), true)
@@ -105,15 +99,7 @@ Citizen.CreateThread(function()
 
            if dist < 1.0 and refill == false then
             Bubsi = 5
-            lib.showTextUI(Config.locales.refilltank, {
-                position = "left-center",
-                icon = 'hand',
-                style = {
-                    borderRadius = 0,
-                    backgroundColor = '#48BB78',
-                    color = 'white'
-                }
-            })
+	    textui(show, Config.locales.refilltank)
 	    if IsControlJustPressed(0, 38) and refill == false then
               callback('Bubsioxygentank', function(tank)
 	      if tank then
@@ -122,7 +108,7 @@ Citizen.CreateThread(function()
             	local Scubatank2 = CreateObject(Scubatank, coords.x + 0.5, coords.y, coords.z, 1, 1, 0)
 		PlaceObjectOnGroundProperly(Scubatank2)
 		if Config.useprogress then
-		Config.Progressbars(10000, Config.locales.progressbars2)
+		Progressbars(10000, Config.locales.progressbars2)
 		end
 	        loadAnimDict( "anim@amb@clubhouse@tutorial@bkr_tut_ig3@" )
 	        TaskPlayAnim( PlayerPedId(), "anim@amb@clubhouse@tutorial@bkr_tut_ig3@", "machinic_loop_mechandplayer", 8.0, 1.0, -1, 2, 0, 0, 0, 0 )
@@ -132,9 +118,10 @@ Citizen.CreateThread(function()
 		palkkio('Bubsitankki')
 		refill = false
 	     elseif tank == false then
-		Config.notification(5000, Config.locales.eioletankki, 'Clientti', Config.types.error)
+		TriggerEvent('Bubsinotify', 5000, Config.locales.eioletankki, Config.types.error)
 	     elseif dist < 1.0 then
-		lib.hideTextUI()
+		textui(hide)
+
              end
 	   end)
 	  end
@@ -158,6 +145,5 @@ Citizen.CreateThread(function()
       EndTextCommandSetBlipName(blip)
    end
 end)
-
 
 
