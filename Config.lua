2d03953011902fcffc19e6ad431e2950c2077363
Bubsi <95187459+AnimeBubsi@users.noreply.github.com>
2024-03-04 13:45:47 +0200

    Update Config.lua
    update

--- Config.lua
+++ Config.lua
@@ -1,5 +1,4 @@
-Config = {}
-
+Config = {} 
 ----[ Framework ]----
 
 Config.Framework = 'esx' -- 'esx' or 'qb'
@@ -18,14 +17,22 @@ Config.vari = 500 -- colour
 
 Config.commonitem = { -- add as many as you want
    "tolkki",
-   "pullo"
+   "pullo",
+   "rkaulakoru",
+   "rsormus",
+   "lasipullo",
+   "sormus"
 }
 
 Config.rareitem = { -- 1 in 100 chance
-   "pulloposti"
+   "pulloposti",
+   "rolex"
 }
 
+
 ----[ Notification ]----
+
+Config.notify = 'brutal' -- brutal, qb, esx
 
 Config.types = { -- Notification types
   error = 'error',
@@ -33,59 +40,15 @@ Config.types = { -- Notification types
   info = 'info'
 }
 
-Config.notification = function(time, text, kumpi, type)
-  if kumpi == 'Clientti' then
-	exports['brutal_notify']:SendAlert("Diving", text, time, type) -- Brutal notify
-	--TriggerEvent('QBCore:Notify', text, type, time) -- qb notify
-   else
-        TriggerClientEvent('brutal_notify:SendAlert', source, 'Diving', text, time, type) -- Brutal notify
-	--TriggerEvent('QBCore:Notify', text, type, time) -- qb notify
-   end
-end
-
 ----[ Progressbar ]----
 
 Config.useprogress = true -- Do you want to use progressbars?
 
-Config.Progressbar = 'sx' -- sx, qb, Buty, Bcall, custom
+Config.Progressbar = 'sx' -- sx, qb, Buty, Bcall, codeforge, Custom (cl_utils)
 
-Config.Progressbars = function(time, text)
- if Config.Progressbar == 'sx' then
-  TriggerEvent('sx-progress', text, 'info', time) -- sx progress
+----[ Textui ]----
 
-  elseif Config.Progressbar == 'qb' then
-
-   exports['progressbar']:Progress({
-   name = "BubsiDiving",
-   duration = time,
-   label = text,
-   useWhileDead = false,
-   canCancel = true,
-   controlDisables = {
-       disableMovement = false,
-       disableCarMovement = false,
-       disableMouse = false,
-       disableCombat = true,
-   }
-})
-
-  elseif Config.Progressbar == 'Buty' then
-
-	exports["Buty-Progress"]:on({
-        time = time, 
-        text = text, 
-        color = "linear-gradient(20.5deg, #AC41DE 9.83%, rgba(172, 65, 222, 0) 93.95%)",
-        color2 = "#AC41DE",
-    }) 
-
-  elseif Config.Progressbar == 'Bcall' then
-
-  exports['BCall_progress']:muestra(time, text)
-
-  elseif Config.Progressbar == 'Custom' then
-
-   end
-end
+Config.textui = 'codeforge' -- ox_lib, okok, codeforge, custom
 
 ----[ Refill ]----
 
